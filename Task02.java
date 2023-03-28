c4b65cb73b73ee9f6a8e9132cbc36692f35ec8f1
Sviatoslav <ecrprague.asi@gmail.com>
2023-03-28 21:54:09 +0300

    T2D

--- Task02.java
+++ Task02.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 /**
@@ -10,18 +8,15 @@ public class Task02 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Сколько элементов хотите ввести? ");
-        int sizeArr = input.nextInt();
 
-        int[] myArray = new int[] { 8, 3, 2, 7, 12, -9, 1, 4 };
-        if (sizeArr % 2 != 0)
-            sizeArr++;
+        int[] myArray = MakeArray(input);
+        input.close();
 
-        
+        System.out.println("Отсортированный массив: ");
 
         for (int j = 0; j < myArray.length; j++) {
 
-            for (int i = (myArray.length - 1) / 2; i >= 0; i--) {
+            for (int i = (myArray.length - 2) / 2; i >= 0; i--) {
                 LargestFromHeap(myArray, i);
             }
             if (myArray[0] != -2147483648) {
@@ -36,7 +31,6 @@ public class Task02 {
         int temp = 0;
 
         if (array[2 * i] >= array[2 * i + 1]) {
-
             if (array[i] < array[2 * i]) {
                 temp = array[i];
                 array[i] = array[2 * i];
@@ -48,6 +42,27 @@ public class Task02 {
                 array[i] = array[2 * i + 1];
                 array[2 * i + 1] = temp;
             }
+        }
+    }
+
+    public static int[] MakeArray(Scanner input) {
+        System.out.println("Сколько элементов хотите ввести? ");
+        int sizeArray = input.nextInt();
+        if (sizeArray % 2 == 0) {
+            int[] madeArray = new int[sizeArray];
+            for (int i = 0; i < madeArray.length; i++) {
+                System.out.printf("Введите элемент %d: ", i + 1);
+                madeArray[i] = input.nextInt();
+            }
+            return madeArray;
+        } else {
+            int[] madeArray = new int[sizeArray + 1];
+            madeArray[sizeArray] = -2_147_483_648;
+            for (int i = 0; i < madeArray.length - 1; i++) {
+                System.out.printf("Введите элемент %d: ", i + 1);
+                madeArray[i] = input.nextInt();
+            }
+            return madeArray;
         }
     }
 }
